fbeacddc7816a052aec3e34de52e69773d4f39c6
ahmed alaa <ahmedalaa96666@gmail.com>
2020-06-19 20:35:45 +0200

    animation js

--- js/main.js
+++ js/main.js
@@ -95,6 +95,26 @@ $(document).ready(function() {
 
 
 
+// animation bubble
+function createBuubles(){
+    
+    const section = document.querySelector('.bubble')
+    const createElement = document.createElement('span')
+    var size = Math.random() * 60 ;
+
+    createElement.style.width =20 +  size +'px';
+    createElement.style.height =20+ size +'px';
+    createElement.style.left = Math.random() * innerWidth +'px';
+    section.appendChild(createElement)
+
+  setTimeout(() => {
+      createElement.remove()
+  }, 4000);
+  
+  
+}
+setInterval(createBuubles,60)
+
 
 
 
