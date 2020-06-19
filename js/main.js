0f50277285e24a013a52d9f6e2dd0f21d709b208
ahmed alaa <ahmedalaa96666@gmail.com>
2020-06-19 20:31:39 +0200

    add bubbles xD

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 const navbtn = document.getElementById('nav-toggle'),
     navBack = document.querySelector('#navback') ,
+    form = document.getElementById('register-form') ,
+    sing = document.getElementById('signup') ,
+    username = document.querySelector('#username') ,
+    email = document.querySelector('#email') ,
+    password = document.querySelector('#password') ,
+    password2 = document.querySelector('#password-confirm') ,
     scrollToTOp = $('.scrolltotop')  ;
     
 
-
+   
 
 // navigation bar
 navbtn.addEventListener('click', function() {
-
 
     navBack.classList.toggle('open');
     this.classList.toggle('open');
@@ -17,36 +22,26 @@ navbtn.addEventListener('click', function() {
 
 
 
-
+// show intro circle
 $(document).ready(function() {
-
-    $('.heading-circle-sec').fadeIn(2000);   
-
-
+    $('.heading-circle-sec').fadeIn(2000);
     setTimeout(() => {
-
         // fadIn circle 
         $('#intro').fadeIn(2000);
-
-        
         setTimeout(() => {
-
             // fadIn logo            
                 $('#logo-intro').show();
-                $('.tlt').textillate({ in: { effect: 'fadeInUpBig' , delay: 90 } });    
-                
+                $('.tlt').textillate({ in: { effect: 'fadeInUpBig' , delay: 90 } });                
                 //fadi in btn 
                 setTimeout(() => {
                     $('.intro-btn').show();
-                    $('.intro-btn').addClass('animate__animated animate__zoomIn');        
+                    $('.intro-btn').addClass('animate__animated animate__zoomIn');
+                }, 2000);
+            }, 2000);          
+        }, 8000);     
+});
 
-                }, 2200);
 
-            }, 2000);
-            
-        }, 8000);
-        
-    });
 
 //   scrolling
 $(window).scroll(function(){
@@ -70,13 +65,6 @@ scrollToTOp.click(function(){
 });
 
 
-
-
-
-
-
-
-
 //form search
 document.querySelector('.search-icon').onclick = function(e) {
     e.preventDefault();
@@ -86,13 +74,94 @@ document.querySelector('.search-icon').onclick = function(e) {
 
 //slider in work page by plugin owl-carousel
 $(document).ready(function() {
-    $(".owl-carousel").owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        },
+    })
 });
+
+
+
+
+
+
+
+
+
+// //get sign in form
+// var signinFormContent = ` <i class="fas fa-times-circle fa-2x "></i>
+// <div class="containerx">
+//     <div class="signin-content">
+//         <div class="signin-image">
+//             <figure><img src="https://thumbs.dreamstime.com/b/dreamcatcher-against-background-colorful-splash-91495859.jpg" class="w-100" alt="sing up image"></figure>
+//         </div>
+
+//         <div class="signin-form">
+//             <h2 class="form-title">Sign in</h2>
+//             <form method="POST" class="register-form" id="login-form">
+//                 <div class="form-group">
+//                     <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+//                     <input type="text" name="your_name" id="your_name" placeholder="Your Name" />
+//                 </div>
+//                 <div class="form-group">
+//                     <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+//                     <input type="password" name="your_pass" id="your_pass" placeholder="Password" />
+//                 </div>
+//                 <div class="form-group">
+//                     <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+//                     <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
+//                 </div>
+//                 <div class="form-group form-button">
+//                     <input type="submit" name="signin" id="signin" class="form-submit" value="Login" />
+//                     <div class="social-login">
+//                         <span class="social-label">Or Login with</span>
+//                         <ul class="socials">
+//                             <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook fab fa-facebook p-1"></i></a></li>
+//                             <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter fab fa-twitter p-1"></i></a></li>
+//                             <li><a href="#"><i class="display-flex-center zmdi zmdi-google fab fa-google p-1"></i></a></li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//                 <p class="pt-2 text-center">Don't have account? <a href="./signup.html" class="text-decoration-none text-info">SingUp</a></p>
+//             </form>
+//         </div>
+//     </div>
+// </div>`
+// $('#userIcon').on('click', function() {
+//     $('.sign-in').html(signinFormContent).css({ "display": "block" })
+//     $('.fa-times-circle').on("click", function() {
+//         $('.sign-in').css({ "display": "none" })
+//     })
+// });
+// $('.loginFromSignupPage').on('click', function() {
+//     $('.sign-in').html(signinFormContent).css({ "display": "block" })
+//     $('.fa-times-circle').on("click", function() {
+//         $('.sign-in').css({ "display": "none" })
+//     })
+// });
+
+
+
+
+
 
 
 //set array for project
 localStorage.setItem("projects", JSON.stringify([{
-        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9c4AIwXXINgJUMS39tddg92pKz-1-DePZ-x77ZQgneqWKHhpS&usqp=CAU",
+        imageURL: "https://mobirise.com/bootstrap-template/assets/images/homepage-template.jpg",
         projectTitle: "title",
         date: "20/11/2019",
         views: 200,
@@ -100,7 +169,7 @@ localStorage.setItem("projects", JSON.stringify([{
         likes: 32
     },
     {
-        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBVIZx1SxmFMk1V33pmBHCxpuNMeVl7v0aAix7PHfHJLoo3FXl&usqp=CAU",
+        imageURL: "https://mobirise.com/bootstrap-template/assets/images/bootstrap-starter-template-492x492.jpg",
         projectTitle: "title",
         date: "10/09/2020",
         views: 580,
@@ -108,7 +177,7 @@ localStorage.setItem("projects", JSON.stringify([{
         likes: 98
     },
     {
-        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9c4AIwXXINgJUMS39tddg92pKz-1-DePZ-x77ZQgneqWKHhpS&usqp=CAU",
+        imageURL: "https://mobirise.com/bootstrap-template/assets/images/agency-template.jpg",
         projectTitle: "title",
         date: "20/11/2019",
         views: 200,
@@ -116,7 +185,7 @@ localStorage.setItem("projects", JSON.stringify([{
         likes: 32
     },
     {
-        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBVIZx1SxmFMk1V33pmBHCxpuNMeVl7v0aAix7PHfHJLoo3FXl&usqp=CAU",
+        imageURL: "https://mobirise.com/bootstrap-4-theme/assets/images/magazine-template.jpg",
         projectTitle: "title",
         date: "10/09/2020",
         views: 580,
@@ -124,7 +193,7 @@ localStorage.setItem("projects", JSON.stringify([{
         likes: 98
     },
     {
-        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9c4AIwXXINgJUMS39tddg92pKz-1-DePZ-x77ZQgneqWKHhpS&usqp=CAU",
+        imageURL: "https://mobirise.com/bootstrap-4-theme/assets/images/business-template.jpg",
         projectTitle: "title",
         date: "20/11/2019",
         views: 200,
@@ -132,14 +201,14 @@ localStorage.setItem("projects", JSON.stringify([{
         likes: 32
     },
     {
-        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBVIZx1SxmFMk1V33pmBHCxpuNMeVl7v0aAix7PHfHJLoo3FXl&usqp=CAU",
+        imageURL: "https://mobirise.com/bootstrap-4-theme/assets/images/restaurant-template.jpg",
         projectTitle: "title",
         date: "10/09/2020",
         views: 580,
         rating: 220,
         likes: 98
     }, {
-        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9c4AIwXXINgJUMS39tddg92pKz-1-DePZ-x77ZQgneqWKHhpS&usqp=CAU",
+        imageURL: "https://mobirise.com/bootstrap-4-theme/assets/images/hotel-template.jpg",
         projectTitle: "title",
         date: "20/11/2019",
         views: 200,
@@ -147,7 +216,7 @@ localStorage.setItem("projects", JSON.stringify([{
         likes: 32
     },
     {
-        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBVIZx1SxmFMk1V33pmBHCxpuNMeVl7v0aAix7PHfHJLoo3FXl&usqp=CAU",
+        imageURL: "https://colorlib.com/wp/wp-content/uploads/sites/2/menztailor-free-template-388x306.jpg",
         projectTitle: "title",
         date: "10/09/2020",
         views: 580,
@@ -173,9 +242,9 @@ function getProjects() {
     var listProjects = '';
     for (let i of projects) {
         listProjects += `
-        <div class="col-md-4 p-4 blogBox moreBox">
-        <div class="card shadow">
-            <img src="${i.imageURL}" class="w-100" alt="">
+        <div class="col-md-4 p-4 projectBox">
+        <div class="card shadow h-100">
+            <img src="${i.imageURL}" class="w-100 border-bottom border-light h-75" alt="">
             <div class="p-3">
                 <h4>${i.projectTitle}</h4>
                 <p>${i.date}</p>
@@ -198,5 +267,5 @@ function getProjects() {
     </div>
         `
     }
-    document.getElementById('projects-cards').innerHTML += listProjects
+    document.getElementById('projects-cards').innerHTML += listProjects 
 }
